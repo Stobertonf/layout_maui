bc11be5a8d9b887108723da0e87b8b83c422c844
stobertonf <stobertonf@gmail.com>
2023-06-24 11:17:22 -0300

    first commit

---
+++ layout_maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace layout_maui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ layout_maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace layout_maui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
